5aeb9e2053a626f84c27e6aa56dc3a2ae05c3112
Aditya Dwivedi <adityaislucky@gmail.com>
2020-06-02 23:14:26 +0530

    Added collectiondetails model at backend

---
+++ LalLabsDataAccessLayer/Models/CollectionDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LalLabsDataAccessLayer.Models
+{
+    public class CollectionDetails
+    {   
+        [Optional]
+        public DateTime? CollectionDate { get; set; }
+
+        [Optional]
+        public string CollectedBy { get; set; }
+
+        [Optional]
+        public decimal? CollectionCharges { get; set; }
+
+        [Optional]
+        public string CollectionAddress { get; set; }
+
+    }
+}
